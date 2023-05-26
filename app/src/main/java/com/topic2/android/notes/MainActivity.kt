bf4b2de7d5ae56daee52d61e055509f4ef07a6aa
NastyaMakeychiik <nasta.rider@gmail.com>
2023-05-26 04:01:45 +0300

    main activity

--- app/src/main/java/com/topic2/android/notes/MainActivity.kt
+++ app/src/main/java/com/topic2/android/notes/MainActivity.kt
@@ -1,21 +1,14 @@
 package com.topic2.android.notes
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.Surface
-import androidx.compose.runtime.Composable
-import com.topic2.android.notes.routing.NotesRouter
-import com.topic2.android.notes.routing.Screen
 import com.topic2.android.notes.theme.NotesTheme
+import com.topic2.android.notes.viewmodel.MainViewModel
 import com.topic2.android.notes.viewmodel.MainViewModelFactory
-import screens.MainViewModel
 import screens.NotesScreen
-import ui.components.SaveNoteScreen
-import ui.components.TrashScreen
-
 
 /**
  * Main activity приложения.
@@ -29,26 +22,16 @@ class MainActivity : AppCompatActivity() {
     )
   })
 
-  @ExperimentalMaterialApi
+  @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
 
-    setContent {
+    setContent() {
       NotesTheme {
-        MainActivityScreen(viewModel = viewModel)
+        NotesScreen(viewModel = viewModel)
       }
     }
   }
 }
-@Composable
-@ExperimentalMaterialApi
-private fun MainActivityScreen(viewModel: MainViewModel){
-  Surface {
-    when (NotesRouter.currentScreen){
-      is Screen.Notes -> NotesScreen(viewModel)
-      is Screen.SaveNote -> SaveNoteScreen(viewModel)
-      is Screen.Trash -> TrashScreen(viewModel)
-    }
 
-  }
-}
+
--- app/src/main/java/com/topic2/android/notes/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/topic2/android/notes/viewmodel/MainViewModel.kt
@@ -9,31 +9,30 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 /**
-
-Модель просмотра, используемая для хранения глобального состояния приложения.*
-Эта модель просмотра используется для всех экранов.
+ * Модель просмотра, используемая для хранения глобального состояния приложения.
+ *
+ * Эта модель просмотра используется для всех экранов.
  */
 class MainViewModel(private val repository: Repository) : ViewModel() {
-    val notesNotInTrash: LiveData<List<NoteModel>> by lazy{
-        repository.getAllNotesInTrash()
+    val notesNotInTrash: LiveData<List<NoteModel>> by lazy {
+        repository.getAllNotesNotInTrash()
     }
 
-    fun onCreateNewNoteClick(){
-
+    fun onCreateNewNoteClick() {
+        //TODO
     }
 
-    fun onNoteClick(note:NoteModel){
+    fun onNoteClick(note: NoteModel) {
 
     }
 
     fun onNoteCheckedChange(
-        note:NoteModel
-    ){
+        note: NoteModel
+    ) {
         viewModelScope.launch(
             Dispatchers.Default
-        ){
+        ) {
             repository.insertNote(note)
         }
     }
-
 }
