f5efe4dcf3089587720dcb4ed16e9a310e7f2cde
NastyaMakeychiik <nasta.rider@gmail.com>
2023-05-26 03:46:35 +0300

    main activity

--- app/src/main/java/com/topic2/android/notes/MainActivity.kt
+++ app/src/main/java/com/topic2/android/notes/MainActivity.kt
@@ -1,110 +1,54 @@
-package screens
+package com.topic2.android.notes
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.topic2.android.notes.data.repository.Repository
-import com.topic2.android.notes.domain.model.ColorModel
-import com.topic2.android.notes.domain.model.NoteModel
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
 import com.topic2.android.notes.routing.NotesRouter
 import com.topic2.android.notes.routing.Screen
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import com.topic2.android.notes.theme.NotesTheme
+import com.topic2.android.notes.viewmodel.MainViewModelFactory
+import screens.MainViewModel
+import screens.NotesScreen
+import ui.components.SaveNoteScreen
+import ui.components.TrashScreen
+
 
 /**
- * View model used for storing the global app state.
- *
- * This view model is used for all screens.
+ * Main activity приложения.
  */
-class MainViewModel(private val repository: Repository) : ViewModel() {
 
-  val notesNotInTrash: LiveData<List<NoteModel>> by lazy {
-    repository.getAllNotesNotInTrash()
-  }
+class MainActivity : AppCompatActivity() {
+  private val viewModel: MainViewModel by viewModels(factoryProducer = {
+    MainViewModelFactory(
+      this,
+      (application as NotesApplication).dependencyInjector.repository
+    )
+  })
 
-  val notesInTrash by lazy { repository.getAllNotesInTrash() }
+  @ExperimentalMaterialApi
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
 
-  private var _noteEntry = MutableLiveData(NoteModel())
-  val noteEntry: LiveData<NoteModel> = _noteEntry
-
-  val colors: LiveData<List<ColorModel>> by lazy {
-    repository.getAllColors()
-  }
-
-
-  private var _selectedNotes = MutableLiveData<List<NoteModel>>(listOf())
-  val selectedNotes: LiveData<List<NoteModel>> = _selectedNotes
-
-  fun onCreateNewNoteClick() {
-    // TODO - Open SaveNoteScreen
-    _noteEntry.value = NoteModel()
-    NotesRouter.navigateTo(Screen.SaveNote)
-  }
-
-  fun onNoteClick(note: NoteModel) {
-    // TODO - Open SaveNoteScreen in Edit mode
-    _noteEntry.value = note
-    NotesRouter.navigateTo(Screen.SaveNote)
-
-  }
-
-  fun onNoteCheckedChange(note: NoteModel) {
-    viewModelScope.launch(Dispatchers.Default) {
-      repository.insertNote(note)
-    }
-  }
-
-  fun onNoteSelected(note: NoteModel) {
-    _selectedNotes.value = _selectedNotes.value!!.toMutableList().apply {
-      if (contains(note)) {
-        remove(note)
-      } else {
-        add(note)
+    setContent {
+      NotesTheme {
+        MainActivityScreen(viewModel = viewModel)
       }
     }
   }
-
-  fun restoreNotes(notes: List<NoteModel>) {
-    viewModelScope.launch(Dispatchers.Default) {
-      repository.restoreNotesFromTrash(notes.map { it.id })
-      withContext(Dispatchers.Main) {
-        _selectedNotes.value = listOf()
-      }
+}
+@Composable
+@ExperimentalMaterialApi
+private fun MainActivityScreen(viewModel: MainViewModel){
+  Surface {
+    when (NotesRouter.currentScreen){
+      is Screen.Notes -> NotesScreen(viewModel)
+      is Screen.SaveNote -> SaveNoteScreen(viewModel)
+      is Screen.Trash -> TrashScreen(viewModel)
     }
-  }
 
-  fun permanentlyDeleteNotes(notes: List<NoteModel>) {
-    viewModelScope.launch(Dispatchers.Default) {
-      repository.deleteNotes(notes.map { it.id })
-      withContext(Dispatchers.Main) {
-        _selectedNotes.value = listOf()
-      }
-    }
-  }
-
-  fun onNoteEntryChange(note: NoteModel) {
-    _noteEntry.value = note
-  }
-
-  fun saveNote(note: NoteModel) {
-    viewModelScope.launch(Dispatchers.Default) {
-      repository.insertNote(note)
-
-      withContext(Dispatchers.Main) {
-        NotesRouter.navigateTo(Screen.Notes)
-        _noteEntry.value = NoteModel()
-      }
-    }
-  }
-
-  fun moveNoteToTrash(note: NoteModel) {
-    viewModelScope.launch(Dispatchers.Default) {
-      repository.moveNoteToTrash(note.id)
-      withContext(Dispatchers.Main) {
-        NotesRouter.navigateTo(Screen.Notes)
-      }
-    }
   }
 }
